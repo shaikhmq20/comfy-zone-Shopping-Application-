9d959b7b4316f28c1c5ee4446eb52b4d3dcb5bd3
shaikhmq20 <87924988+shaikhmq20@users.noreply.github.com>
2021-08-25 23:00:08 +0530

    Adding background image for the banner.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import Header from "./components/header";
+import Banner from "./components/banner";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <Header />;
+    return (
+      <React.Fragment>
+        <Header />
+        <Banner />
+      </React.Fragment>
+    );
   }
 }
 
---
+++ src/components/banner.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import banner from "../images/banner.jpg";
+
+class Banner extends Component {
+  render() {
+    return <div id="banner"></div>;
+  }
+}
+
+export default Banner;
