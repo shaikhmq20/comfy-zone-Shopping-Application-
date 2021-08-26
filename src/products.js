e10aee4e5968495f028b77740df307cdf69b57fd
shaikhmq20 <87924988+shaikhmq20@users.noreply.github.com>
2021-08-26 18:11:22 +0530

    Adding id for all the products.

--- src/products.js
+++ src/products.js
@@ -1,43 +1,51 @@
 const products = [
   {
     name: "Queen Panel Bed",
-    price: 10.99,
+    price: 100.99,
     image: "../images/img1 (Custom).jpg",
+    id: 1,
   },
   {
     name: "King Panel Bed",
-    price: 12.99,
+    price: 120.99,
     image: "../images/img2 (Custom).jpg",
+    id: 2,
   },
   {
     name: "Single Panel Bed",
-    price: 12.99,
+    price: 120.99,
     image: "../images/img3 (Custom).jpg",
+    id: 3,
   },
   {
     name: "Twin Panel Bed",
-    price: 22.99,
+    price: 220.99,
     image: "../images/img4 (Custom).jpg",
+    id: 4,
   },
   {
     name: "Fridge",
-    price: 88.99,
+    price: 880.99,
     image: "../images/img5 (Custom).jpg",
+    id: 5,
   },
   {
     name: "Dresser",
-    price: 32.99,
+    price: 320.99,
     image: "../images/img6 (Custom).jpg",
+    id: 6,
   },
   {
     name: "Couch",
-    price: 45.99,
+    price: 450.99,
     image: "../images/img7 (Custom).jpg",
+    id: 7,
   },
   {
     name: "Table",
-    price: 33.99,
+    price: 330.99,
     image: "../images/img8 (Custom).jpg",
+    id: 8,
   },
 ];
 
