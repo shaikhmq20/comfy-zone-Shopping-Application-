9a0ef08bc23514a8253f7941e6fa708ab9a4b2e8
Qasim Shaikh <87924988+shaikhmq20@users.noreply.github.com>
2022-07-26 22:15:38 +0530

    adding icons for counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+
 class Counter extends Component {
   render() {
     return (
@@ -10,7 +13,7 @@ class Counter extends Component {
             onClick={(cprod) => this.props.onDecrement(cprod)}
             disabled={this.props.cprod.count === 0}
           >
-            -
+            <FontAwesomeIcon icon={faMinus} />
           </button>
         </div>
         <div className="count">{this.props.cprod.count}</div>
@@ -19,7 +22,7 @@ class Counter extends Component {
             type="increase"
             onClick={(cprod) => this.props.onIncrement(cprod)}
           >
-            +
+            <FontAwesomeIcon icon={faPlus} />
           </button>
         </div>
       </div>
