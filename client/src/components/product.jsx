5af9c89fc5409e219f79e33e2a95bcb036d8c756
shaikhmq20 <shaikhmq20.comp@coep.ac.in>
2023-03-26 08:58:33 +0530

    getting products from the api

--- client/src/components/product.jsx
+++ client/src/components/product.jsx
@@ -3,24 +3,29 @@ import React, { Component } from "react";
 import { addItem } from "../utils/cartUtil";
 
 class Product extends Component {
-
   async onClicking(item) {
     await addItem(item);
   }
 
   render() {
-    const { image, name, price } = this.props.product;
+    const { images: image, title: name, price } = this.props.product;
     return (
       <React.Fragment>
         <div className="product-container">
-          <div className="product-image">
-            <img src={image} alt="" />
+          <div
+            className="product-image"
+            style={{ backgroundImage: `url(${image[0]})`, backgroundSize: "cover"}}
+          >
+            {/* <img src={image[0]} alt="" /> */}
           </div>
           <div className="product-name">{name}</div>
-          <div className="product-price">₹ {price}</div>
-          <span onClick={() => {
-            this.onClicking(this.props.product);
-          }} className="add-to-cart">
+          <div className="product-price">$ {price}</div>
+          <span
+            onClick={() => {
+              this.onClicking(this.props.product);
+            }}
+            className="add-to-cart"
+          >
             <i id="add-to-cart" class="fas fa-shopping-cart"></i> ADD TO CART
           </span>
         </div>
--- client/src/components/productsDisplay.jsx
+++ client/src/components/productsDisplay.jsx
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
 import Product from "./product";
 import Cart from "./cart";
+import axios from "axios";
+
 class ProductsDisplay extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const products = await axios
+      .get("https://dummyjson.com/products/?limit=100")
+      .then((res) => res.data)
+      .then((data) => this.setState({ products: data.products }));
+  }
+
   render() {
     return (
       <React.Fragment>
         <h3>Our Products</h3>
         <div id="products-container">
-          {this.props.products.map((product) => {
-            return (
-              <Product
-                product={product}
-                key={product.id}
-              />
-            );
+          {this.state.products.map((product) => {
+            return <Product product={product} key={product.id} />;
           })}
         </div>
-        <Cart/>
+        <Cart />
       </React.Fragment>
     );
   }
