9ba813a94239cb937706b2973157a93b4fab2768
shaikhmq20 <shaikhmq20.comp@coep.ac.in>
2023-03-26 23:05:49 +0530

    adding public_url

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route
             path="/"
