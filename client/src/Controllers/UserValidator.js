cd099ff52e8dc6ee5eb68f3f89b813aae467ebe6
shaikhmq20 <shaikhmq20.comp@coep.ac.in>
2023-03-28 12:57:30 +0530

    formatting code

--- client/src/Controllers/UserValidator.js
+++ client/src/Controllers/UserValidator.js
@@ -1,29 +1,31 @@
-const VALIDATOR_TYPE_REQUIRE = 'REQUIRE';
-const VALIDATOR_TYPE_MINLENGTH = 'MINLENGTH';
-const VALIDATOR_TYPE_MAXLENGTH = 'MAXLENGTH';
-const VALIDATOR_TYPE_MIN = 'MIN';
-const VALIDATOR_TYPE_MAX = 'MAX';
-const VALIDATOR_TYPE_EMAIL = 'EMAIL';
-const VALIDATOR_TYPE_FILE = 'FILE';
-const VALIDATOR_TYPE_MIS = 'MIS';
-const VALIDATOR_TYPE_EXACT='EXACT'
+const VALIDATOR_TYPE_REQUIRE = "REQUIRE";
+const VALIDATOR_TYPE_MINLENGTH = "MINLENGTH";
+const VALIDATOR_TYPE_MAXLENGTH = "MAXLENGTH";
+const VALIDATOR_TYPE_MIN = "MIN";
+const VALIDATOR_TYPE_MAX = "MAX";
+const VALIDATOR_TYPE_EMAIL = "EMAIL";
+const VALIDATOR_TYPE_FILE = "FILE";
+const VALIDATOR_TYPE_MIS = "MIS";
+const VALIDATOR_TYPE_EXACT = "EXACT";
 
 export const VALIDATOR_REQUIRE = () => ({ type: VALIDATOR_TYPE_REQUIRE });
 export const VALIDATOR_FILE = () => ({ type: VALIDATOR_TYPE_FILE });
-export const VALIDATOR_MINLENGTH = val => ({
+export const VALIDATOR_MINLENGTH = (val) => ({
   type: VALIDATOR_TYPE_MINLENGTH,
-  val: val
+  val: val,
 });
-export const VALIDATOR_MAXLENGTH = val => ({
+export const VALIDATOR_MAXLENGTH = (val) => ({
   type: VALIDATOR_TYPE_MAXLENGTH,
-  val: val
+  val: val,
 });
-export const VALIDATOR_MIN = val => ({ type: VALIDATOR_TYPE_MIN, val: val });
-export const VALIDATOR_MAX = val => ({ type: VALIDATOR_TYPE_MAX, val: val });
+export const VALIDATOR_MIN = (val) => ({ type: VALIDATOR_TYPE_MIN, val: val });
+export const VALIDATOR_MAX = (val) => ({ type: VALIDATOR_TYPE_MAX, val: val });
 export const VALIDATOR_EMAIL = () => ({ type: VALIDATOR_TYPE_EMAIL });
-export const VALIDATOR_MIS = val => ({ type: VALIDATOR_TYPE_MIS, val: val});
-export const VALIDATOR_EXACT=val=>({type:VALIDATOR_TYPE_EXACT,val:val});
-
+export const VALIDATOR_MIS = (val) => ({ type: VALIDATOR_TYPE_MIS, val: val });
+export const VALIDATOR_EXACT = (val) => ({
+  type: VALIDATOR_TYPE_EXACT,
+  val: val,
+});
 
 export const validate = (value, validators) => {
   let isValid = true;
@@ -46,12 +48,17 @@ export const validate = (value, validators) => {
     if (validator.type === VALIDATOR_TYPE_EMAIL) {
       isValid = isValid && /^\S+@\S+\.\S+$/.test(value);
     }
-    if (validator.type === VALIDATOR_TYPE_MIS){
-      isValid = isValid && /^[0-9]+$/.test(value) && value.trim().length === validator.val;
+    if (validator.type === VALIDATOR_TYPE_MIS) {
+      isValid =
+        isValid &&
+        /^[0-9]+$/.test(value) &&
+        value.trim().length === validator.val;
     }
-    if(validator.type===VALIDATOR_TYPE_EXACT){
-        isValid = isValid && /^[0-9]+$/.test(value) && value.trim().length === validator.val;
-
+    if (validator.type === VALIDATOR_TYPE_EXACT) {
+      isValid =
+        isValid &&
+        /^[0-9]+$/.test(value) &&
+        value.trim().length === validator.val;
     }
   }
   return isValid;
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,25 +1,19 @@
 import React from "react";
 import axios from "axios";
-import {  VALIDATOR_EMAIL,VALIDATOR_EXACT } from "../Controllers/UserValidator";
+import { VALIDATOR_EMAIL, VALIDATOR_EXACT } from "../Controllers/UserValidator";
 import { useForm } from "../Controllers/useFormhook";
-import {Button ,Card} from "@material-ui/core";
+import { Button, Card } from "@material-ui/core";
 import Input from "../Controllers/Input";
 import { useHistory } from "react-router-dom";
 import Header from "./header";
 
+const Login = (props) => {
+  const capitalize = (str, lower = false) =>
+    (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
+      match.toUpperCase()
+    );
 
-
-const Login=(props)=>{
-    const capitalize = (str, lower = false) =>
-  (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
-    match.toUpperCase()
-  );
-  
-
-  const history=useHistory();
-
- 
-
+  const history = useHistory();
 
   const [formState, inputHandler] = useForm(
     {
@@ -36,95 +30,90 @@ const Login=(props)=>{
   );
 
   const handleSubmit = (event) => {
-        
+    event.preventDefault();
+  };
 
-      event.preventDefault();
-      
-
-      
-       
-    }
-
-    const apicall=()=>{
-      const url = `${window.location.origin}/api/login/`;
-        axios.post(url+"userExist", {Email: formState.inputs.Email.value,
-        Password: formState.inputs.Password.value
-  }).then((res) => {
-          if(res.data.message=="Login Successfull"){
-            history.push("/home");
-            
-
-          }
-          else{
-            alert(res.data.message);
-          }
-          
-            
-          });
-        
-    }
-    return (
-      <>
-      <Header darkMode={props.darkMode}
-      toggleTheme={() =>props.toggleTheme()}/>
+  const apicall = () => {
+    const url = `${window.location.origin}/api/login/`;
+    axios
+      .post(url + "userExist", {
+        Email: formState.inputs.Email.value,
+        Password: formState.inputs.Password.value,
+      })
+      .then((res) => {
+        if (res.data.message == "Login Successfull") {
+          history.push("/home");
+        } else {
+          alert(res.data.message);
+        }
+      });
+  };
+  return (
+    <>
+      <Header
+        darkMode={props.darkMode}
+        toggleTheme={() => props.toggleTheme()}
+      />
       <center>
-        <Card className="authentication" style={{ backgroundColor: "#D1D7E0",width:"50%",height:"70%" ,marginTop:"20%"}}>
-      
-        <h2
+        <Card
+          className="authentication"
           style={{
-            backgroundColor: "#2D283E",
-            margin: "0",
-            color: "#D1D7E0",
-            padding: "10px",
+            backgroundColor: "#D1D7E0",
+            width: "50%",
+            height: "70%",
+            marginTop: "20%",
           }}
         >
-          Login Required
-        </h2>
-        
-        <hr style={{ backgroundColor: "#333996", margin: "0" }} />
-        
-          
-        
-
-      <form onSubmit={handleSubmit} style={{ backgroundColor: "#D1D7E0",padding:"3%" }}>
-    
-    
-          <Input
-            element="input"
-            id="Email"
-            type="Email"
-            label="Email"
-            validators={[VALIDATOR_EMAIL(10)]}
-            errorText="Please enter a valid Email with minimum 10 characters."
-            onInput={inputHandler}
-          />
-          <Input
-            element="input"
-            id="Password"
-            type="Password"
-            label="Password"
-            validators={[VALIDATOR_EXACT(9)]}
-            errorText="Please enter a valid Password with 9 digits."
-            onInput={inputHandler}
-          />
-          <Button
-            variant="contained"
-            style={{ backgroundColor: "#2D283E",color:"white" }}
-            type="submit"
-            disabled={!formState.isValid}
-            sx={{ marginTop: "10px" }}
-            onClick={apicall}
+          <h2
+            style={{
+              backgroundColor: "#2D283E",
+              margin: "0",
+              color: "#D1D7E0",
+              padding: "10px",
+            }}
           >
-            SUBMIT
-          </Button>
+            Login Required
+          </h2>
 
-      </form>
-      </Card>
+          <hr style={{ backgroundColor: "#333996", margin: "0" }} />
+
+          <form
+            onSubmit={handleSubmit}
+            style={{ backgroundColor: "#D1D7E0", padding: "3%" }}
+          >
+            <Input
+              element="input"
+              id="Email"
+              type="Email"
+              label="Email"
+              validators={[VALIDATOR_EMAIL(10)]}
+              errorText="Please enter a valid Email with minimum 10 characters."
+              onInput={inputHandler}
+            />
+            <Input
+              element="input"
+              id="Password"
+              type="Password"
+              label="Password"
+              validators={[VALIDATOR_EXACT(9)]}
+              errorText="Please enter a valid Password with 9 digits."
+              onInput={inputHandler}
+            />
+            <Button
+              variant="contained"
+              style={{ backgroundColor: "#2D283E", color: "white" }}
+              type="submit"
+              disabled={!formState.isValid}
+              sx={{ marginTop: "10px" }}
+              onClick={apicall}
+            >
+              SUBMIT
+            </Button>
+          </form>
+        </Card>
       </center>
-      </>
-    )
-}
+    </>
+  );
+};
 
 export default Login;
-
- 
