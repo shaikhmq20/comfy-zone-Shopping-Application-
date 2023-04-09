7841d9cfd9d3f4b662369fd332a1283b7851c6f1
shaikhmq20 <shaikhmq20.comp@coep.ac.in>
2023-04-09 18:22:54 +0530

    Minor Fix

--- client/src/components/cartItem.jsx
+++ client/src/components/cartItem.jsx
@@ -8,43 +8,43 @@ class CartItem extends Component {
     const { title, thumbnail, price, id } = this.props.cprod;
 
     return (
-      <Link to={`/product/${id}`} style={{
-        textDecoration: "none",
-      }}>
-        <div className="cart-item">
-          <div
-            className="item-image"
+      <div className="cart-item">
+        <div
+          className="item-image"
+          style={{
+            backgroundImage: `url(${thumbnail})`,
+            backgroundSize: "cover",
+            borderRadius: "8px",
+          }}>
+          {/* <img src={images[0]} alt={title} /> */}
+        </div>
+        <div className="item-details">
+          <Link
+            to={`/product/${id}`}
             style={{
-              backgroundImage: `url(${thumbnail})`,
-              backgroundSize: "cover",
-              borderRadius: "8px",
-            }}
-          >
-            {/* <img src={images[0]} alt={title} /> */}
-          </div>
-          <div className="item-details">
+              textDecoration: "none",
+            }}>
             <div className="item-name">{title}</div>
-            <div className="item-price">${price}</div>
-            <div id="response">
-              <div className="item-count">
-                <Counter
-                  cprod={this.props.cprod}
-                  onIncrement={(cprod) => this.props.onIncrement(cprod)}
-                  onDecrement={(cprod) => this.props.onDecrement(cprod)}
-                />
-              </div>
-              <div className="remove-item">
-                <button
-                  className="remove-btn"
-                  onClick={(cprod) => this.props.onRemove(cprod.id)}
-                >
-                  Remove
-                </button>
-              </div>
+          </Link>
+          <div className="item-price">${price}</div>
+          <div id="response">
+            <div className="item-count">
+              <Counter
+                cprod={this.props.cprod}
+                onIncrement={(cprod) => this.props.onIncrement(cprod)}
+                onDecrement={(cprod) => this.props.onDecrement(cprod)}
+              />
+            </div>
+            <div className="remove-item">
+              <button
+                className="remove-btn"
+                onClick={(cprod) => this.props.onRemove(cprod.id)}>
+                Remove
+              </button>
             </div>
           </div>
         </div>
-      </Link>
+      </div>
     );
   }
 }
